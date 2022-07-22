aebcb2cf9a2be8de96b281c02ad7939509c5b90c
kk <1213099875@qq.com>
2022-07-22 10:55:31 +0800

    login-form-wave

---
+++ 09_login-form-wave/index.js
@@ -0,0 +1,7 @@
+const labels = document.querySelectorAll('.form-item label')
+labels.forEach(label => {
+  label.innerHTML = label.innerHTML
+    .split('').map((letter, index) =>
+      `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+    ).join("")
+})
